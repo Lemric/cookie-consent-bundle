f68a9048ce1a7de5e0624cdd7ac469387da4fce7
Oliver Witzki <oliver.witzki@chrono24.com>
2023-12-25 00:43:48 +0100

    Use FormData in cookie-consent.mjs to submit data

--- Resources/assets/js/cookie-consent.mjs
+++ Resources/assets/js/cookie-consent.mjs
@@ -1,65 +1,50 @@
 document.addEventListener("DOMContentLoaded", function () {
     const cookieConsent = document.querySelector('.cookie-consent');
     const cookieConsentForm = document.querySelector('.cookie-consent__form');
-    const cookieConsentFormBtn = document.querySelectorAll('.js-submit-cookie-consent-form');
+
     if (cookieConsentForm) {
+        // we got a form
+        const submitButtons = cookieConsentForm.querySelectorAll('.js-submit-cookie-consent-form');
         const formAction = cookieConsentForm.action ? cookieConsentForm.action : location.href;
 
         const cookieConsentDialog = document.querySelector('.cookie-consent-dialog');
         if (cookieConsentDialog) {
+            // we got a dialog, show it
             cookieConsentDialog.showModal();
-
-            const saveButton = cookieConsentDialog.querySelector('#cookie_consent_save');
-            if (saveButton) {
-                saveButton.addEventListener('click', function () {
-                    cookieConsentDialog.close();
-                });
-            }
-
-            cookieConsentDialog.querySelectorAll('.js-reject-all-cookies').forEach(function (rejectButton) {
-                rejectButton.addEventListener('click', function () {
-                    // parse form and send information about rejection to set only minimal cookies
-                    fetch(formAction, {
-                        method: 'POST',
-                        headers: {
-                            'Content-Type': 'application/x-www-form-urlencoded'
-                        },
-                        body: [getCsrfToken(cookieConsentForm), encodeURIComponent(rejectButton.getAttribute('name')) + "="].join('&')
-                    }).then(function (res) {
-                        cookieConsentDialog.close();
-                        cookieConsent.remove();
-                    }).catch(function (error) {
-                        console.error('Error:', error);
-                    });
-                });
-            });
         }
 
+        cookieConsentForm.querySelectorAll('.js-reject-all-cookies').forEach(function (rejectButton) {
+            rejectButton.addEventListener('click', function () {
+                // reject all was clicked!
+                // parse form and send information about rejection to set only minimal cookies
+                fetch(formAction, {
+                    method: 'POST',
+                    body: new FormData(cookieConsentForm, rejectButton)
+                }).then(function (res) {
+                    if (res.status >= 200 && res.status < 300) {
+                        hideCookieConsentForm(cookieConsent, cookieConsentDialog);
+                        dispatchSuccessEvent(rejectButton);
+                    }
+                }).catch(function (error) {
+                    console.error('Error:', error);
+                });
+            });
+        });
+
         // Submit form via ajax
-        cookieConsentFormBtn.forEach(function (btn) {
-            btn.addEventListener('click', function (event) {
+        submitButtons.forEach(function (button) {
+            button.addEventListener('click', function (event) {
                 event.preventDefault();
 
                 document.querySelector('.js-reject-all-cookies').disabled = true;
 
                 fetch(formAction, {
                     method: 'POST',
-                    headers: {
-                        'Content-Type': 'application/x-www-form-urlencoded'
-                    },
-                    body: serializeForm(cookieConsentForm, event.target)
+                    body: new FormData(cookieConsentForm, event.target)
                 }).then(function (res) {
                     if (res.status >= 200 && res.status < 300) {
-                        if (cookieConsentDialog) {
-                            cookieConsentDialog.close();
-                        } else {
-                            cookieConsent.remove();
-                        }
-
-                        const formSubmittedEvent = new CustomEvent('cookie-consent-form-submit-successful', {
-                            detail: event.target
-                        });
-                        document.dispatchEvent(formSubmittedEvent);
+                        hideCookieConsentForm(cookieConsent, cookieConsentDialog);
+                        dispatchSuccessEvent(event.target);
                     }
                 }).catch(function (error) {
                     console.error('Error:', error);
@@ -69,22 +54,16 @@ document.addEventListener("DOMContentLoaded", function () {
     }
 });
 
-function getCsrfToken(cookieConsentForm) {
-    let csrfTokenField = cookieConsentForm.querySelector('#cookie_consent__token');
-    return encodeURIComponent(csrfTokenField.getAttribute('name')) + '=' + encodeURIComponent(csrfTokenField.getAttribute('value'));
+function dispatchSuccessEvent(submitter) {
+    const formSubmittedEvent = new CustomEvent('cookie-consent-form-submit-successful', {
+        detail: submitter
+    });
+    document.dispatchEvent(formSubmittedEvent);
 }
 
-function serializeForm(form, clickedButton) {
-    const serialized = [];
-
-    Array.from(form.elements).forEach(function (field) {
-        if (field.classList.contains('js-cookie-consent-form-element') && field.checked) {
-            serialized.push(encodeURIComponent(field.name) + "=" + encodeURIComponent(field.value));
-        }
-    });
-
-    serialized.push(encodeURIComponent(clickedButton.getAttribute('name')) + "=");
-    serialized.push(getCsrfToken(form));
-
-    return serialized.join('&');
+function hideCookieConsentForm(cookieConsent, cookieConsentDialog) {
+    if (cookieConsentDialog) {
+        cookieConsentDialog.close();
+    }
+    cookieConsent.remove();
 }
